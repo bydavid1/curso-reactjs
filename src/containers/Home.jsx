d03f14dfbe3132f469210bcae5f70dda87596345
Byron Jimenez <byronjimenez9911@gmail.com>
2020-11-29 12:57:59 -0600

    React-router-dom installed

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -10,7 +10,7 @@ import useInitialState from '../hooks/useInitialState'
 
 const API = "http://localhost:3000/initalState"
 
-const App = () => {
+const Home = () => {
     const [videos, categories] = useInitialState(API)
     return(
         <div className="App">
@@ -31,4 +31,4 @@ const App = () => {
     )
 }
 
-export default App
+export default Home
