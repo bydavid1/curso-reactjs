c5ecead96f20434a3cdd97e61b45ee97e829c3f3
Byron Jimenez <byronjimenez9911@gmail.com>
2020-12-28 21:05:48 -0600

    Webpack and files minifier plugins

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const webpack = require('webpack');
 //const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 require('dotenv').config();
 
@@ -9,7 +11,7 @@ const isDev = (process.env.ENV === 'development');
 const entry = ['./src/frontend/index.js'];
 
 if (isDev) {
-  entry.push('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true')
+  entry.push('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true');
 }
 
 module.exports = {
@@ -23,6 +25,10 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
   module: {
     rules: [
       {
@@ -32,14 +38,14 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
+      // {
+      //   test: /\.html$/,
+      //   use: [
+      //     {
+      //       loader: 'html-loader',
+      //     },
+      //   ],
+      // },
       {
         test: /\.(s*)css$/,
         use: [
@@ -68,6 +74,10 @@ module.exports = {
   },
   plugins: [
     isDev ? new webpack.HotModuleReplacementPlugin() : () => { },
+    isDev ? () => {} : new CompressionWebpackPlugin({
+      test: /\.js$|\.css$/,
+      filename: '[path].gz',
+    }),
     // new HtmlWebpackPlugin({
     //   template: "./public/index.html",
     //   filename: "./index.html",
